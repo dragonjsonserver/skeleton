4a0e862fb42821bcfa06c0aefc2ac921f6fcb149
Christoph Herrmann <developer@dragonprojects.de>
2013-05-05 11:39:46 +0200

    Mehrere Eventarten unterstützen und "tick" als Default nehmen

--- data/database/install.sql
+++ data/database/install.sql
@@ -122,6 +122,7 @@ CREATE TABLE `gamerounds` (
 	`bot` BOOLEAN NOT NULL,
 	`progress` INTEGER(11) NOT NULL,
 	`active` BOOLEAN NOT NULL,
+	`event` VARCHAR(255) NOT NULL,
 	PRIMARY KEY (`gameround_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
