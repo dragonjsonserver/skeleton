9d01b02daaa67194cae55ae412b3421ff9cf2808
Christoph Herrmann <developer@dragonprojects.de>
2013-05-06 21:32:09 +0200

    Erweiterung für den DragonJsonServer zur Verwaltung von Herausforderungen

--- config/application.config.php
+++ config/application.config.php
@@ -14,6 +14,7 @@ return [
     'modules' => [   
         'DragonJsonServer',
         'DragonJsonServerAccount',
+        'DragonJsonServerAchievement',
         'DragonJsonServerAlliance',
         'DragonJsonServerApiannotation',
         'DragonJsonServerAvatar',
