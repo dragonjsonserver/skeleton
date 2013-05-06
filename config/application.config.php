6dd1613697d82a6e6f27029607ba2865716b0f1a
Christoph Herrmann <developer@dragonprojects.de>
2013-05-06 22:14:19 +0200

    Erweiterung für den DragonJsonServer zur Verwaltung von Accountherausforderungen

--- config/application.config.php
+++ config/application.config.php
@@ -14,6 +14,7 @@ return [
     'modules' => [   
         'DragonJsonServer',
         'DragonJsonServerAccount',
+        'DragonJsonServerAccountachievement',
         'DragonJsonServerAchievement',
         'DragonJsonServerAlliance',
         'DragonJsonServerApiannotation',
--- data/database/install.sql
+++ data/database/install.sql
@@ -15,6 +15,18 @@ CREATE TABLE `sessions` (
 	UNIQUE KEY `sessionhash` (`sessionhash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `accountachievements` (
+	`accountachievement_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`modified` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+	`created` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`account_id` BIGINT(20) UNSIGNED NOT NULL,
+	`gamedesign_identifier` VARCHAR(255) NOT NULL,
+	`data` TEXT NOT NULL,
+	`level` INTEGER(11) NOT NULL,
+	PRIMARY KEY (`accountachievement_id`),
+	UNIQUE KEY `account_id` (`account_id`, `gamedesign_identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `alliances` (
 	`alliance_id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`modified` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
